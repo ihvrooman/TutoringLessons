3b22cf63ececfbfc161db88fb77d82de8d196783
Isaac Vrooman <61886068+ihvrooman@users.noreply.github.com>
2022-08-29 16:03:36 -0400

    Added weather library.

---
+++ WeatherLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherLibrary
+{
+    public class Class1
+    {
+        /* This is where you should create your classes, services,
+         * and other shared code for your weather application.
+         */
+    }
+}
